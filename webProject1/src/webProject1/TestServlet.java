c0cf0813e5b426900855ad5751e10709b6df390b
yoon <yoon@yoon-PC>
2018-01-05 16:38:07 +0900

    최초생성

---
+++ webProject1/src/webProject1/TestServlet.java
@@ -0,0 +1,58 @@
+package webProject1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+	       response.setContentType("text/html;charset = utf-8");
+	         PrintWriter out = response.getWriter();
+	         
+	         out.println("<html>");
+	         out.println("<head>");
+	         out.println("<title> Servlet 시작 </title>");
+	         out.println("</head>");
+	         out.println("<body>");
+	         out.println("<h1> 처음 시작하는 Servlet 입니다");
+	         out.println("</body>");
+	         out.println("</html>");
+	         out.close();
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
